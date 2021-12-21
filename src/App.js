ee09ec706438cc5eeaa6ddb259b7ce04565c0468
Melissa <wolfaholic@users.noreply.github.com>
2021-12-20 20:04:54 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <h2>Client Auth Project</h2>
+      <h2>Client Auth Projects</h2>
     </div>
   );
 }
