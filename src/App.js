2c74a63a7e382ccc183b16bd687f156ac6e4555a
Melissa <wolfaholic@users.noreply.github.com>
2021-12-20 20:27:49 -0800

    added routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
+
+const Login = ()=> {
+  return(<h2>Login</h2>)
+}
+
+const FriendsList = ()=> {
+  return(<h2>FriendsList</h2>)
+}
+const AddFriend = ()=> {
+  return(<h2>FriendsList</h2>)
+}
 
 function App() {
   return (
+    <Router>
     <div className="App">
+      <Route exact path = "/">
+        <Login />
+      </Route>
+      <Route exact path = "/login">
+        <Redirect to="/" />
+      </Route>
+      <Route exact path = "/friends">
+        <FriendsList />
+      </Route>
+      <Route exact path = "/friends/add">
+        <AddFriend />
+      </Route>
       <h2>Client Auth Projects</h2>
     </div>
+    </Router>
   );
 }
 
